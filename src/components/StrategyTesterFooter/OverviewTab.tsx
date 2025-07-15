0020f6c3aca01eb6a1eb02beeca223fd5242694f
Nuelchi <edubem80@gmail.com>
2025-07-15 16:58:50 +0100

    fix

--- src/components/StrategyTesterFooter/OverviewTab.tsx
+++ src/components/StrategyTesterFooter/OverviewTab.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
 
-const OverviewTab = ({ metrics }: { metrics: any }) => (
-  <div>
-    <h3 className="text-lg font-semibold mb-2">Overview</h3>
-    <pre className="bg-muted/20 rounded p-3 text-xs overflow-auto">{JSON.stringify(metrics, null, 2)}</pre>
-  </div>
-);
-
-export default OverviewTab; 
+export default function OverviewTab() {
+  return <div>OverviewTab Placeholder</div>;
+} 
